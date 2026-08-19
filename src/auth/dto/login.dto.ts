13178837d47d49285ad3880517e1953ec0f75cee
unknown <migueloliveiraaleixosantos1@gmail.com>
2026-08-18 22:25:13 -0300

    Feito segurança

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -3,10 +3,10 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 export class LoginDto {
   @IsEmail({}, { message: 'Informe um email válido' })
   @IsNotEmpty({ message: 'O email é obrigatório' })
-  email?: string;
+  email!: string;
 
   @IsString({ message: 'A senha deve ser um texto' })
   @IsNotEmpty({ message: 'A senha é obrigatória' })
   @MinLength(6, { message: 'A senha deve ter no mínimo 8 caracteres' })
-  senha?: string;
+  senha!: string;
 }
